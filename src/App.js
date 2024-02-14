434828fe330a7bff94d93d0728eee3b8d6e5bc1b
zora-git <154859133+zora-git@users.noreply.github.com>
2024-02-14 17:27:00 +0900

    added footer

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <h1>Dictionary</h1>
+      <header className="App-header"></header>
+      <main> Hello </main>
+      <footer className="App-footer">
+        <small>Coded by HJ Park</small>
+        <div className="Logo-section">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+      </footer>
     </div>
   );
 }
