073286020f8fea75625c74886738ab543912e7f0
zora-git <154859133+zora-git@users.noreply.github.com>
2024-02-14 17:05:11 +0900

    added spinning name logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import logo from "./name-in-arc.svg";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">hello</header>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <h1>Dictionary</h1>
     </div>
   );
 }
