8a1d3a49f495fdb748aaec98d29334086be126b3
zora-git <154859133+zora-git@users.noreply.github.com>
2024-02-15 19:12:22 +0900

    added synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
+
       <p> {props.meaning.definition}</p>
-      <p> {props.meaning.example}</p>
+      <p>
+        {" "}
+        Example:
+        <em>{props.meaning.example}</em>
+      </p>
+
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
