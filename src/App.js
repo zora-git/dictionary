bbea01ba32a9e10cb0312d996607982d6199a3b7
zora-git <154859133+zora-git@users.noreply.github.com>
2024-02-17 15:34:20 +0900

    made responsive cursor

--- src/App.js
+++ src/App.js
@@ -4,34 +4,38 @@ import AnimatedCursor from "react-animated-cursor";
 
 export default function App() {
   return (
-    <div className="App">
-      <AnimatedCursor
-        innerSize={30}
-        outerSize={70}
-        color="116, 154, 195"
-        outerAlpha={0.2}
-        innerScale={0.7}
-        outerScale={2}
-        trailingSpeed={4}
-        clickables={[
-          "a",
-          'input[type="search"]',
-          'input[type="submit"]',
-          'input[type="image"]',
-          "select",
-          "textarea",
-          "button",
-          ".link",
-        ]}
-      />
-      <header className="App-header"></header>
+    <div>
+      <div className="d-none d-md-block">
+        <AnimatedCursor
+          innerSize={30}
+          outerSize={70}
+          color="116, 154, 195"
+          outerAlpha={0.2}
+          innerScale={0.7}
+          outerScale={2}
+          trailingSpeed={4}
+          clickables={[
+            "a",
+            'input[type="search"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            "select",
+            "textarea",
+            "button",
+            ".link",
+          ]}
+        />
+      </div>
+      <div className="App">
+        <header className="App-header"></header>
 
-      <main>
-        <h1>
-          <a href="/">Dictionary</a>
-        </h1>{" "}
-        <Dictionary />{" "}
-      </main>
+        <main>
+          <h1>
+            <a href="/">Dictionary</a>
+          </h1>{" "}
+          <Dictionary />{" "}
+        </main>
+      </div>
     </div>
   );
 }
