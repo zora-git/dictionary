f3aee762b6f6f5bd37a2921fed57afd9a7fac739
zora-git <154859133+zora-git@users.noreply.github.com>
2024-02-17 14:51:05 +0900

    added cursor animation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
+import AnimatedCursor from "react-animated-cursor";
 
 export default function App() {
   return (
     <div className="App">
+      <AnimatedCursor
+        innerSize={30}
+        outerSize={70}
+        color="116, 154, 195"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={2}
+        trailingSpeed={4}
+        clickables={[
+          "a",
+          'input[type="search"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          "select",
+          "textarea",
+          "button",
+          ".link",
+        ]}
+      />
       <header className="App-header"></header>
 
       <main>
