8e0862b7d7c197001d463d93d2e8ead7b5c622e6
zora-git <154859133+zora-git@users.noreply.github.com>
2024-02-19 13:40:56 +0900

    simplified event button

--- src/Button.js
+++ src/Button.js
@@ -1,40 +1,13 @@
-import { useState, useEffect } from "react";
 import "./Button.css";
 
 export default function Button() {
-  const [color, setColor] = useState("#282C34");
-  const [isActive, setIsActive] = useState(false);
-
-  function buttonColorChange() {
-    setIsActive(true);
+  function changeStyle() {
+    document.body.classList.add("light");
   }
-
-  const click = (color) => {
-    setColor(color);
-  };
-
-  let newcolor = "#e0e0e0";
-
-  useEffect(() => {
-    document.body.style.backgroundColor = color;
-  }, [color]);
 
   return (
     <div>
-      <button
-        style={{
-          backgroundColor: isActive ? "#e0e0e0" : ``,
-          color: isActive ? `#749ac3` : ``,
-        }}
-        onClick={() => {
-          click(newcolor);
-          buttonColorChange();
-        }}
-        className="Button"
-      >
-        {" "}
-        Enlighten Me{" "}
-      </button>
+      <button onClick={changeStyle}> Enlighten Me </button>
     </div>
   );
 }
